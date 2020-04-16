2279973733357f627aa3612febb365228c1cee60
Flyewzz <dentalon599@gmail.com>
2020-04-17 01:24:58 +0300

    A Breed is in the pet's picture, line spaces were decreased

--- vk-app-finder/src/components/cards/AnimalCard.js
+++ vk-app-finder/src/components/cards/AnimalCard.js
@@ -1,26 +1,40 @@
 import React from "react";
 import Card from "@vkontakte/vkui/dist/components/Card/Card";
-import "./AnimalCard.css"
-import config from '../../config';
-import {Cell} from "@vkontakte/vkui";
+import "./AnimalCard.css";
+import config from "../../config";
+import { Div } from "@vkontakte/vkui";
 
 const AnimalCard = (props) => {
   const animal = props.animal;
-  const breed = animal.breed === ''? 'порода не указана': animal.breed;
-  const date = new Date(animal.date.replace(' ', 'T')).toLocaleDateString();
+  const breed = animal.breed === "" ? "порода не указана" : animal.breed;
+  const date = new Date(animal.date.replace(" ", "T"))
+    .toLocaleDateString()
+    .replace(/\//g, ".");
   console.log(props.address);
 
   return (
-    <Card onClick={props.onClick} style={{height: 315}}
-          className="animal__card" size="m" mode="shadow">
-      <img className={'animal-card__photo'}
-           src={config.baseUrl + `lost/img?id=${animal.picture_id}`}
-           height='180px' alt={''}/>
-      <Cell className={'animal-card__address'}>{props.address}</Cell>
-      <Cell className={'animal-card__info'}>
-        {`${config.types[animal.type_id - 1]}, ${breed}`}
-      </Cell>
-      <Cell className={'animal-card__date'}>{date}</Cell>
+    <Card
+      onClick={props.onClick}
+      style={{ height: "max(315px, auto)" }}
+      className="animal__card"
+      size="m"
+      mode="shadow"
+    >
+      <div className={"animal-card__photo__container"}>
+        <div className={"animal-card__photo__corner"}>
+          <div className={"animal-card__photo__breed"}>{breed}</div>
+        </div>
+        <img
+          className={"animal-card__photo"}
+          src={config.baseUrl + `lost/img?id=${animal.picture_id}`}
+          height="180px"
+          alt={""}
+        />
+      </div>
+      <Div className={"animal-card__data"}>
+        <p className={"animal-card__address"}>{props.address}</p>
+        <p className={"animal-card__date"}>Дата пропажи: {date}</p>
+      </Div>
     </Card>
   );
 };
--- vk-app-finder/src/components/profile/ProfileCard.js
+++ vk-app-finder/src/components/profile/ProfileCard.js
@@ -1,49 +1,63 @@
-import React from 'react';
-import Card from '@vkontakte/vkui/dist/components/Card/Card';
-import './ProfileCard.css';
-import Icon28CancelOutline from '@vkontakte/icons/dist/28/cancel_outline';
-import config from '../../config';
-import {Group, List} from "@vkontakte/vkui";
-import {Cell} from "@vkontakte/vkui/dist/es6";
+import React from "react";
+import Card from "@vkontakte/vkui/dist/components/Card/Card";
+import "./ProfileCard.css";
+import Icon28CancelOutline from "@vkontakte/icons/dist/28/cancel_outline";
+import config from "../../config";
+import { Group, List } from "@vkontakte/vkui";
+import { Cell } from "@vkontakte/vkui/dist/es6";
+import { autorun } from "mobx";
 
 const ProfileCard = (props) => {
   const animal = props.animal;
-  const breed = animal.breed === '' ? 'порода не указана' : animal.breed;
-  const date = new Date(animal.date.replace(' ', 'T')).toLocaleDateString();
+  const breed = animal.breed === "" ? "порода не указана" : animal.breed;
+  const date = new Date(animal.date.replace(" ", "T")).toLocaleDateString();
 
   return (
     <Card
       onClick={() => props.onClick(animal.id)}
-      style={{height: 190}}
-      className={'profile__card__container'}
+      style={{
+        height: "max(190, auto)",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "stretch",
+      }}
+      className={"profile__card__container"}
       size="l"
       mode="shadow"
     >
-      <div style={{display: 'flex', height: '100%', width: '100%'}}>
-        <img
-          className={'profile__card__photo'}
-          src={config.baseUrl + `lost/img?id=${animal.picture_id}`}
-          alt={""}
-        />
-        <Group className={'profile__card__details'}>
-          <List>
-            <Icon28CancelOutline onClick={() => props.cancel(animal.id)}
-                                 className={'profile__cancel-icon'}/>
-            <Cell className={'profile__card__info'}>
-              <span style={{fontWeight: 'bold'}}>Потерялся: </span>
-              {`${config.types[animal.type_id - 1]}, ${breed}`}
-            </Cell>
-            <Cell>
-              <span style={{fontWeight: 'bold'}}>Адрес: </span>
+      <div
+        style={{ display: "flex", flexGrow: 1, height: "100%", width: "100%" }}
+      >
+        <div className={"profile__card__photo__container"}>
+          <div className={"profile__card__photo__corner"}>
+            <div className={"profile__card__photo__breed"}>{breed}</div>
+          </div>
+          <img
+            className={"profile__card__photo"}
+            src={config.baseUrl + `lost/img?id=${animal.picture_id}`}
+            alt={""}
+          />
+        </div>
+        <Group className={"profile__card__details"}>
+          <List className={"profile__attributes_list"}>
+            <Icon28CancelOutline
+              onClick={() => props.cancel(animal.id)}
+              className={"profile__cancel-icon"}
+            />
+            <div className={"profile__card__info_cell"}>
+              <span style={{ fontWeight: "bold" }}>Адрес: </span>
               {props.address}
-            </Cell>
-            <Cell>
-              <span style={{fontWeight: 'bold'}}>Дата пропажи: </span>{date}
-            </Cell>
-            <Cell size="m">
-              <span style={{fontWeight: 'bold'}}>Описание: </span>
+            </div>
+
+            <div className={"profile__card__info_cell"}>
+              <span style={{ fontWeight: "bold" }}>Дата пропажи: </span>
+              {date}
+            </div>
+
+            <div className={"profile__card__info_cell"}>
+              <span style={{ fontWeight: "bold" }}>Описание: </span>
               {animal.description}
-            </Cell>
+            </div>
           </List>
         </Group>
       </div>
