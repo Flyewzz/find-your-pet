4f5c87126a3584968e9f8435231790607019b64c
Flyewzz <dentalon599@gmail.com>
2020-04-19 09:48:43 +0300

    Lost list supports empty pictures

--- vk-app-finder/src/panels/LostAnimalPanel.js
+++ vk-app-finder/src/panels/LostAnimalPanel.js
@@ -1,21 +1,22 @@
 import React from "react";
-import {CellButton, Div, Group, Header, PanelHeader} from "@vkontakte/vkui";
-import {Cell, PanelHeaderBack, Separator} from "@vkontakte/vkui/dist/es6";
-import LostService from '../services/LostService';
-import {decorate, observable, runInAction} from "mobx";
-import {observer} from "mobx-react";
-import './Lost.css';
+import { CellButton, Div, Group, Header, PanelHeader } from "@vkontakte/vkui";
+import { Cell, PanelHeaderBack, Separator } from "@vkontakte/vkui/dist/es6";
+import LostService from "../services/LostService";
+import { decorate, observable, runInAction } from "mobx";
+import { observer } from "mobx-react";
+import "./Lost.css";
 import config from "../config";
 import List from "@vkontakte/vkui/dist/components/List/List";
 import InfoRow from "@vkontakte/vkui/dist/components/InfoRow/InfoRow";
 import Avatar from "@vkontakte/vkui/dist/components/Avatar/Avatar";
-import Icon24Write from '@vkontakte/icons/dist/24/write';
-import Icon24Done from '@vkontakte/icons/dist/24/done';
-import Icon24Share from '@vkontakte/icons/dist/24/share';
-import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
-import './LostAnimalPanel.css';
+import Icon24Write from "@vkontakte/icons/dist/24/write";
+import Icon24Done from "@vkontakte/icons/dist/24/done";
+import Icon24Share from "@vkontakte/icons/dist/24/share";
+import Icon24Cancel from "@vkontakte/icons/dist/24/cancel";
+import "./LostAnimalPanel.css";
 import ProfileService from "../services/ProfileService";
 import GeocodingService from "../services/GeocodingService";
+import vk_dog from "../img/vk_dog.jpg";
 
 class LostAnimalPanel extends React.Component {
   constructor(props) {
@@ -25,78 +26,81 @@ class LostAnimalPanel extends React.Component {
     this.geocodingService = new GeocodingService();
   }
 
-  animal = {date: ''};
-  address = '';
+  animal = { date: "" };
+  address = "";
   // TODO add default avatar
-  author = {first_name: '', last_name: '', photo_50: ''};
+  author = { first_name: "", last_name: "", photo_50: "" };
   isMine = false;
 
   componentDidMount() {
-    this.props.userStore.getId().then(resultId =>
+    this.props.userStore.getId().then((resultId) =>
       this.lostService.getById(this.props.id).then(
-        result => {
+        (result) => {
           runInAction(() => {
             this.animal = result;
             this.isMine = this.animal.vk_id === resultId.id;
           });
           this.getAddress();
-          this.props.userStore.getUserById(this.animal.vk_id).then(
-            result => {
-              runInAction(() => {
-                this.author = result.response[0];
-              })
+          this.props.userStore.getUserById(this.animal.vk_id).then((result) => {
+            runInAction(() => {
+              this.author = result.response[0];
             });
+          });
         },
-        error => {
+        (error) => {
           alert(error);
-        })
+        }
+      )
     );
   }
 
   getAddress = () => {
-    const {latitude, longitude} = this.animal;
-    this.geocodingService.addressByCoords(longitude, latitude).then(
-      response => {
+    const { latitude, longitude } = this.animal;
+    this.geocodingService
+      .addressByCoords(longitude, latitude)
+      .then((response) => {
         const result = response.address;
 
-        let address = result.City === '' ? result.District : result.City;
-        if (result.Address !== '') {
-          address += ', ' + result.Address;
+        let address = result.City === "" ? result.District : result.City;
+        if (result.Address !== "") {
+          address += ", " + result.Address;
         }
-        if (result.MetroArea !== '') {
-          address += ', ' + result.MetroArea;
+        if (result.MetroArea !== "") {
+          address += ", " + result.MetroArea;
         }
 
         this.address = address;
-      }
-    );
+      });
   };
 
   sexText = {
-    'm': 'Мужской',
-    'f': 'Женский',
-    'n/a': 'Не указан',
+    m: "Мужской",
+    f: "Женский",
+    "n/a": "Не указан",
   };
 
   writeHref = () => {
     if (this.author.can_write_private_message) {
       return {
         href: `https://vk.com/im?sel=${this.animal.vk_id}`,
-        description: 'Напишите автору, если у Вас есть информация о животном'
+        description: "Напишите автору, если у Вас есть информация о животном",
       };
     }
     return {
       href: `https://vk.com/id${this.animal.vk_id}`,
-      description: `Отправить заявку на добавление в друзья`
+      description: `Отправить заявку на добавление в друзья`,
     };
   };
 
   getShareText = () => {
-    const {type_id} = this.animal;
-    const breed = this.animal.breed === '' ? 'порода не указана' : this.animal.breed;
+    const { type_id } = this.animal;
+    const breed =
+      this.animal.breed === "" ? "порода не указана" : this.animal.breed;
     // TODO getting address from coords
-    return `Потерян питомец: ${config.types[type_id - 1]}, ${breed}. ` +
+    return (
+      `Потерян питомец: ${config.types[type_id - 1]}, ${breed}. ` +
       `Адрес: ${this.address}. ${config.appUrl}`
+    );
   };
 
   share = () => {
@@ -105,84 +109,121 @@ class LostAnimalPanel extends React.Component {
 
   onClose = () => {
     const vkId = this.props.userStore.id;
-    this.profileService.closeLost(this.props.id, vkId).then(
-      () => this.props.goBack()
-    )
+    this.profileService
+      .closeLost(this.props.id, vkId)
+      .then(() => this.props.goBack());
   };
 
   render() {
-    const {picture_id, type_id, description} = this.animal;
-    const breed = this.animal.breed === '' ? ' Порода не указана' : this.animal.breed;
-    const date = new Date(this.animal.date.replace(' ', 'T')).toLocaleDateString();
+    const { picture_id, type_id, description } = this.animal;
+    const breed =
+      this.animal.breed === "" ? " Порода не указана" : this.animal.breed;
+    const date = new Date(
+      this.animal.date.replace(" ", "T")
+    ).toLocaleDateString();
     const type = config.types[type_id - 1];
 
     return (
       <>
-        <PanelHeader left={<PanelHeaderBack onClick={this.props.goBack}/>}>
+        <PanelHeader left={<PanelHeaderBack onClick={this.props.goBack} />}>
           Потерянный питомец
         </PanelHeader>
         <Group separator="hide">
           <Div>
-            { picture_id != undefined &&
-            <img style={{width: '100%'}}
-                 src={config.baseUrl + `lost/img?id=${picture_id}`} alt={''}/>
-            }
+            
+              <img
+                style={{ width: "100%" }}
+                src={
+                  picture_id
+                    ? (config.baseUrl + `lost/img?id=${picture_id}`)
+                    : vk_dog
+                }
+                alt={""}
+              />
           </Div>
-          <div style={{display: 'flex', alignItems: 'center'}}>
-            <Header style={{paddingBottom: '10px'}}>{type + ', ' + breed}</Header>
-            <Div onClick={this.share}
-                 style={{cursor: 'pointer', display: 'flex', alignItems: 'center', marginLeft: 'auto'}}>
-              {'Поделиться'}<Icon24Share style={{marginLeft: '5px'}}/>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Header style={{ paddingBottom: "10px" }}>
+              {type + ", " + breed}
+            </Header>
+            <Div
+              onClick={this.share}
+              style={{
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+                marginLeft: "auto",
+              }}
+            >
+              {"Поделиться"}
+              <Icon24Share style={{ marginLeft: "5px" }} />
             </Div>
           </div>
-          <Group header={!this.isMine && <Header mode={'secondary'}>Автор</Header>}>
-          { !this.isMine ? (
-            [
-            <Cell
-              before={<Avatar src={this.author.photo_50}/>}>
-              {this.author.first_name + ' ' + this.author.last_name}
-            </Cell>,
-            <Cell multiline={true} description={this.writeHref().description}>
-              <CellButton href={this.writeHref().href}
-                          target={'_blank'}
-                          before={<Icon24Write/>} className={'author__action-button'}>Написать</CellButton>
-            </Cell>,
-            <Cell multiline={true} description={'Или дайте ему знать, мы пришлем ему уведомление'}>
-              <CellButton before={<Icon24Done/>} className={'author__action-button'}>Я нашел!</CellButton>
-            </Cell>
-            ]) : ([
-            <CellButton before={<Icon24Write/>}>
-              Изменить объявление
-            </CellButton>,
-            <CellButton before={<Icon24Cancel/>}
-                        onClick={() => this.props.openDestructive(this.onClose)}
-                        mode={'danger'}>
-              Закрыть объявление
-            </CellButton>]
-            )}
+          <Group
+            header={!this.isMine && <Header mode={"secondary"}>Автор</Header>}
+          >
+            {!this.isMine
+              ? [
+                  <Cell before={<Avatar src={this.author.photo_50} />}>
+                    {this.author.first_name + " " + this.author.last_name}
+                  </Cell>,
+                  <Cell
+                    multiline={true}
+                    description={this.writeHref().description}
+                  >
+                    <CellButton
+                      href={this.writeHref().href}
+                      target={"_blank"}
+                      before={<Icon24Write />}
+                      className={"author__action-button"}
+                    >
+                      Написать
+                    </CellButton>
+                  </Cell>,
+                  <Cell
+                    multiline={true}
+                    description={
+                      "Или дайте ему знать, мы пришлем ему уведомление"
+                    }
+                  >
+                    <CellButton
+                      before={<Icon24Done />}
+                      className={"author__action-button"}
+                    >
+                      Я нашел!
+                    </CellButton>
+                  </Cell>,
+                ]
+              : [
+                  <CellButton before={<Icon24Write />}>
+                    Изменить объявление
+                  </CellButton>,
+                  <CellButton
+                    before={<Icon24Cancel />}
+                    onClick={() => this.props.openDestructive(this.onClose)}
+                    mode={"danger"}
+                  >
+                    Закрыть объявление
+                  </CellButton>,
+                ]}
           </Group>
-          <Separator/>
+          <Separator />
           <Group>
             <List>
               <Cell>
-                <InfoRow header="Место пропажи">
-                  {this.address}
-                </InfoRow>
+                <InfoRow header="Место пропажи">{this.address}</InfoRow>
               </Cell>
               <Cell>
                 <InfoRow header="Пол животного">
                   {this.sexText[this.animal.sex]}
                 </InfoRow>
               </Cell>
-              {description !== '' && <Cell multiline={true}>
-                <InfoRow header="Описание">
-                  {description}
-                </InfoRow>
-              </Cell>}
+              {description !== "" && (
+                <Cell multiline={true}>
+                  <InfoRow header="Описание">{description}</InfoRow>
+                </Cell>
+              )}
               <Cell>
-                <InfoRow header="Дата создания объявления">
-                  {date}
-                </InfoRow>
+                <InfoRow header="Дата создания объявления">{date}</InfoRow>
               </Cell>
             </List>
           </Group>
