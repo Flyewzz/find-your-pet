01299b54c29ce5476d3281e3cf19f7cd54fcf250
Flyewzz <dentalon599@gmail.com>
2020-04-28 20:51:28 +0300

    Replaced mcsrv with flask

--- vk-app-finder/main.py
+++ vk-app-finder/main.py
@@ -1,36 +1,24 @@
 from concurrent import futures
 import logging
 
-import grpc
-import classifier_pb2
-import classifier_pb2_grpc
 import classifier
-
 from breed_formatter import *
+import json
+from flask import Flask, request
 
-class BreedClassifier(classifier_pb2_grpc.BreedClassifierServiceServicer):
+app = Flask(__name__)
 
-    def RecognizeBreed(self, request, context):
-        if request.path != '':
-            print('*** Gotten ***')
-        # img = Image.open(BytesIO(base64.b64decode(request.path)))
-        breed = classifier.Xception_predict_breed(request.path)
-        print('breed: %s' % breed)
-        # Give a formatted breed (eng)
-        breed = breed_format(breed)
-        return classifier_pb2.Breed(name=breed)
-    
-    
-def serve():
-    breed_classifier = BreedClassifier()
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    classifier_pb2_grpc.add_BreedClassifierServiceServicer_to_server(breed_classifier, server)
-    server.add_insecure_port('[::]:50051')
-    server.start()
-    server.wait_for_termination()
-
+@app.route('/breed', methods=['POST'])
+def index():
+    img = request.json['picture']
+    breed = classifier.Xception_predict_breed(img)
+    print('breed: %s' % breed)
+    # Give a formatted breed (eng)
+    breed = breed_format(breed)
+    # Only one breed right now (*TODO* FIX IT)
+    return json.dumps([breed], ensure_ascii=False).encode('utf8')
 
 if __name__ == '__main__':
     logging.basicConfig()
     print('*** Breed classificator is ready ***')
-    serve()
+    app.run(host='0.0.0.0', port=5000, debug=True)
