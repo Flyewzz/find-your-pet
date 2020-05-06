eb209fdaec1793f319f040bf85f032d68af35067
Kotyarich <kotovnd@mail.ru>
2020-05-06 00:57:47 +0000

    Fix import name

--- extract_bottleneck_features.py
+++ extract_bottleneck_features.py
@@ -1,3 +1,3 @@
 def extract_Xception(tensor):
-	from keras.applications.xception import Xception, preprocess_input
+	from tensorflow.keras.applications.xception import Xception, preprocess_input
 	return Xception(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
